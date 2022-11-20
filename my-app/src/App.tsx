59e93c558cf95e802b29e6de1251f9e468695273
jburzy <jackson.carl.burzynski@cern.ch>
2022-11-20 14:03:39 -0800

    Add navbar

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World!  
-        </p>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
 
---
+++ my-app/src/components/Navbar/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import {FaBars} from 'react-icons/fa'
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from './NavbarElements'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to='/'>Jackson Burzynski</NavLogo>
+                <MobileIcon>
+                  <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                  <NavItem>
+                    <NavLinks to="about">About</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to="research">Research</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to="projects">Projects</NavLinks>
+                  </NavItem>
+                </NavMenu>
+                <NavBtn>
+                  <NavBtnLink to='/resume'>Resume</NavBtnLink>
+                </NavBtn>
+            </NavbarContainer>
+        </Nav>
+    </>
+  )
+}
+
+export default Navbar;
